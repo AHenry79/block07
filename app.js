4b38de54848fb7f3491fea17dde613a265b8a3c1
AHenry79 <austinhenry7799@gmail.com>
2024-04-15 14:00:29 -0400

    Calculations are numbers, not strings

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 const notification = "You have received this message because you have been chosen to open an important vault. Here is the secret combination";
 
 // Then, I'll add variables containing the equations where, if solved, reveals the answer to the combination
-const comb1 = "20 / 2";
-const comb2 = "10 * 4";
-const comb3 = "49 - 10";
+const comb1 = 20 / 2;
+const comb2 = 10 * 4;
+const comb3 = 49 - 10;
 
 // Finally, I'll write an alert to make a popup say the strings
-alert(notification+" \nCombination 1: "+comb1+"\nCombination 2: "+comb2+"\nCombination 3: "+comb3);
+alert(notification+" Combination 1: "+comb1+"Combination 2: "+comb2+"Combination 3: "+comb3);
