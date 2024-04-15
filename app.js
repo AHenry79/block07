f2a1f224538f7044bf925bd87ac1072c975347d8
AHenry79 <austinhenry7799@gmail.com>
2024-04-15 13:39:23 -0400

    Javascript Practice

---
+++ app.js
@@ -0,0 +1,11 @@
+// How I will solve the exercise:
+// I will make a variable to house the string with the message of the secret code
+const notification = "You have received this message because you have been chosen to open an important vault. Here is the secret combination";
+
+// Then, I'll add variables containing the equations where, if solved, reveals the answer to the combination
+const comb1 = "20 / 2";
+const comb2 = "10 * 4";
+const comb3 = "49 - 10";
+
+// Finally, I'll write an alert to make a popup say the strings
+alert(notification+" \nCombination 1: "+comb1+"\nCombination 2: "+comb2+"\nCombination 3: "+comb3);
